443cdb0251b094749e25083e8812104ec8a12d04
kekelele996 <kekelele996@users.noreply.github.com>
2026-08-18 01:26:45 +0800

    fix: warehouse-picking-state-nil-err-0002

--- internal/model/model.go
+++ internal/model/model.go
@@ -70,7 +70,7 @@ var transitions = map[Status][]Status{
 	StatusAssigned:  {StatusPicking, StatusFailed},
 	StatusPicking:   {StatusCompleted, StatusFailed},
 	StatusFailed:    {StatusRetrying},
-	StatusRetrying:  {},
+	StatusRetrying:  {StatusPicking},
 	StatusCompleted: {},
 }
 
